1c6eac1bed1e685f85421a0605858e0aedd915f3
Rupert Acuesta <rupertacuesta@gmail.com>
2021-01-02 13:03:40 +0000

    coursework V4
    changes to the main drawGrid function for readability and a more efficient approach

--- 1st Year Coursework/2016499.py
+++ 1st Year Coursework/2016499.py
@@ -11,68 +11,64 @@ def getInputs():
         if size == "5" or size == "7":
             break
         print("invalid input, enter again")
-    colours = "red green blue magenta orange cyan grey white"
-    splitColours = colours.split() #split into list of strings
+    validColours = "red green blue magenta orange cyan"
+    splitColours = validColours.split() #split into list of strings
     while True:
         colourNum = 1
-        colour1 = validColour(splitColours, colourNum)
+        colour1 = checkValid(splitColours, colourNum)
         colourNum += 1
-        colour2 = validColour(splitColours, colourNum)
+        colour2 = checkValid(splitColours, colourNum)
         colourNum += 1
-        colour3 = validColour(splitColours, colourNum)
-        if colour1 == colour2 == colour3:
+        colour3 = checkValid(splitColours, colourNum)
+        if colour1 == colour2 == colour3: #must be 1 different colour
             print("invalid, there must be at least 1 different colour")
         else:
             break
-    return size, colour1, colour2, colour3
+    return int(size), colour1, colour2, colour3
 
-def validColour(splitColours, colourNum):
+def checkValid(splitColours, colourNum):
     notValid = True
     while notValid:
         inputColour = input("enter colour {0}: ".format(colourNum))
-        for eachColour in splitColours:
-            if inputColour == eachColour:
+        for colour in splitColours: #loop through each colour
+            if inputColour == colour:
                 notValid = False
                 break
         if notValid == True:
             print("invalid colour, enter again")
     return inputColour
-
+# blue green red
 def drawGrid(size, colour1, colour2, colour3):
-    intSize = int(size)
-    win = GraphWin("window", intSize*100, intSize*100)
-    newSize = int((intSize+1)/2) #diagonal squares
-    downSquares = 2
-    upSquares = intSize - 1
+    win = GraphWin("window", size*100, size*100)
     x = 0
-    y = (intSize*100) - 100
-    for finalPatch in range(newSize):
-        finalNumberPatch(win, x, y, colour2)
-        if finalPatch > 0: #add patches downwards aside from 1st box
-            penNumPatch(win, x-100, y+100, colour2)
+    y = (size*100) - 100
+    bottomSquares = 1
+    topSquares = size - 1
+    for diagonalSquare in range(size):
+        if(diagonalSquare % 2) == 0:
+            finalNumPatch(win, x, y, colour2)
+        else:
+            penNumPatch(win, x, y, colour2)
+        if diagonalSquare > 0:
             downY = y + 100
-            downY2 = y + 200
-            for downFinPatch in range(downSquares):
-                finalNumberPatch(win, x, downY, colour3)
+            for bottomSide in range(bottomSquares):
+                if(bottomSquares % 2) == 0:
+                    finalNumPatch(win, x, downY, colour3)
+                else:
+                    penNumPatch(win, x, downY, colour3)
                 downY += 100
-            for downFinPatch2 in range(downSquares-1):
-                penNumPatch(win, x-100, downY2, colour3)
-                downY2 += 100
-            downSquares += 2
-        upY = y - 100 #add patches upwards aside from last top right box
-        upY2 = y - 200
-        for upPenPatch in range(upSquares):
-            penNumPatch(win, x, upY, colour1)
-            upY -= 100
-        for upPenPatch2 in range(upSquares-1):
-            penNumPatch(win, x+100, upY2, colour1)
-            upY2 -= 100
-        upSquares -= 2
-        x += 200
-        y -= 200
-    time.sleep(5)
+            bottomSquares += 1
+        if diagonalSquare < size:
+            upY = y - 100
+            for topSide in range(topSquares):
+                penNumPatch(win, x, upY, colour1)
+                upY -= 100
+            topSquares -= 1
+        x += 100
+        y -= 100
+    time.sleep(3)
 
-def finalNumberPatch(win, x, y, colour):
+def finalNumPatch(win, x, y, colour):
     topY = y - 10 #only these 2 dimensions change
     bottomX = x + 110
     for i in range(5):
@@ -82,16 +78,18 @@ def finalNumberPatch(win, x, y, colour):
         bottomX -= 20
 
 def penNumPatch(win, x, y, colour):
-    circleY = y
+    circleY = y #circles going downwards
     for circleRow in range(3):
-        circleX = x
+        circleX = x #circles going to the right
         for circle in range(3):
             drawCircle(win, circleX, circleY, colour)
             drawTriangle(win, circleX, circleY, 20)
             if circle < 2:
-                drawSquare(win, circleX+20, circleY, circleX+40, circleY+20, colour)
+                drawSquare(win, circleX+20, circleY,
+                circleX+40, circleY+20, colour)
             if circleRow < 2:
-                drawSquare(win, circleX, circleY+20, circleX+20, circleY+40, colour)
+                drawSquare(win, circleX, circleY+20,
+                circleX+20, circleY+40, colour)
             if circleRow < 2 and circle < 2:
                 drawCircle(win, circleX+20, circleY+20, colour)
                 drawTriangle(win, circleX+20, circleY+20, 0)
@@ -101,14 +99,16 @@ def penNumPatch(win, x, y, colour):
 def drawCircle(win, x, y, colour):
     circle = Circle(Point(x+10, y+10), 10)
     circle.setFill(colour)
+    circle.setOutline(colour)
     circle.draw(win)
 
 def drawSquare(win, x, y, x2, y2, colour):
     square = Rectangle(Point(x, y), Point(x2, y2))
     square.setFill(colour)
+    square.setOutline(colour)
     square.draw(win)
 
-def drawTriangle(win, x, y, directionX):
+def drawTriangle(win, x, y, directionX): #directionX determines whether arrow faces left or right
     triangle = Polygon(Point(x+directionX, y),
     Point(x+10, y+10),
     Point(x+directionX, y+20))
